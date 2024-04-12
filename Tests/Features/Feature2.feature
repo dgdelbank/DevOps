030e8a27229a6e8936e75a29b5210e4e9a36f3ad
dg-delbank <douglasdelbank@gmail.com>
2024-04-12 17:07:31 -0300

    dont work

---
+++ Tests/Features/Feature2.feature
@@ -0,0 +1,7 @@
+﻿Feature: devopsTest
+
+@CT02
+Scenario: @CT0s_Verify first item from list
+	Given Access to data endpoints
+	When Get item with ids = 1
+	Then Item has to be nameds 'Item 1'
---
+++ Tests/Support/TestPage.cs
@@ -0,0 +1,21 @@
+﻿using RestSharp;
+
+namespace Tests.Support
+{
+    internal class TestPage
+    {
+        private RestClient _client;
+        private RestResponse _response;
+
+        public void AccessEndpoint()
+        {
+            _client = new RestClient("http://localhost:5000");
+        }
+
+        public void GetItem()
+        {
+            var request = new RestRequest("api/data", Method.Get);
+            _response = _client.Execute(request);
+        }
+    }
+}
